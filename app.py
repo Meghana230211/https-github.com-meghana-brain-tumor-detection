6f1f85605c2135d4b592ddc471eeecd6cbe61b26
SAARANIS <158195213+SAARANIS@users.noreply.github.com>
2024-07-13 07:32:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "model")
 import streamlit as st
 from pages._pages import home
 from pages._pages import about
-from pages._pages import github
+
 from pages._pages import try_it
 
 routes = {
